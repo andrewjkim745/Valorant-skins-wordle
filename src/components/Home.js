f68c0ed17b7a34ebdbb7c1b4792886aa2065fde6
Andrew Kim <andrewk745@gmail.com>
2022-03-22 02:06:51 -0400

    setup home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect } from 'react'
+
+
+
+export default function Home() {
+
+    const [ inputValue, setInputValue ] = useState('')
+
+
+    return (
+        <div className='bg-dark'>
+        Valordle!
+        </div>
+    )
+
+}
