99dbd14a17d0519cc2dc6a563ab7fbdb9ee34657
Andrew Kim <andrewk745@gmail.com>
2022-03-22 02:35:17 -0400

    added code to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,9 @@ export default function Home() {
 
 
     return (
-        <div className='bg-dark'>
-        <h1>Valordle!</h1>
+        <div className='w-100 vh-100 bg-dark'>
+        <h1 className='text-white text-center'>Valordle!</h1>
+        <h2 className='text-white text-center'>A Valorant Skins Wordle Game by <a href='https://github.com/andrewjkim745'>andrewjkim745</a></h2>
         </div>
     )
 
