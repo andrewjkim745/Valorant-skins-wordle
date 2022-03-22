9b181747786f854c213aa8e5d21629b815a314c6
Andrew Kim <andrewk745@gmail.com>
2022-03-22 01:59:15 -0400

    installed bootstrap, sass dependencies and skins data

---
+++ src/assets/skinsData.js
@@ -0,0 +1,30 @@
+
+
+
+
+export const skins = [
+    {
+        weapon: 'phantom',
+        collection: 'Oni',
+        price: '1750',
+        rarity: 'epic'
+    },
+    {
+        weapon: 'vandal',
+        collection: 'Reaver',
+        price: '2250',
+        rarity: 'legendary'
+    },
+    {
+        weapon: 'operator',
+        collection: 'forsaken',
+        price: '1750',
+        rarity: 'epic'
+    },
+    {
+        weapon: 'spectre',
+        collection: 'prime',
+        price: '1550',
+        rarity: 'normal'
+    }
+]
