a7c705286b9a994efc2fb310d6a436046ec1486e
Andrew Kim <andrewk745@gmail.com>
2022-03-27 05:29:24 -0400

    installed webpack

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { MDBInput } from 'mdb-react-ui-kit';
 import Dropdown from './Dropdown/Dropdown';
 import Button from './Button/Button';
@@ -6,33 +6,31 @@ import Button from './Button/Button';
 
 export default function Home() {
 
-    const [ inputValue, setInputValue ] = useState('')
-    const [ skinInfo, setSkinInfo ] = useState(true)
-    const [ hint, setShowHint ] = useState(true)
+    const [inputValue, setInputValue] = useState('')
+    const [skinInfo, setSkinInfo] = useState(true)
+    const [hint, setShowHint] = useState(true)
 
 
     return (
         <div className='w-100 vh-100 bg-dark'>
-        <h1 className='text-white text-center'>Valordle!</h1>
-        <h2 className='text-white text-center'>A Valorant Skins Wordle Game by <a href='https://github.com/andrewjkim745'>andrewjkim745</a></h2>
-        <p className='text-white text-center'>Updates at 00:00 GMT-10</p>
-        <div className='w-100 d-flex flex-column justify-content-center align-items-center'>
-        <MDBInput onChange={(e)=>setInputValue(e.target.value)} value={inputValue} label='Weapon Name' id='form1' type='text' contrast />
-        <Dropdown/>
-        <Button
-        toggle={'toggle'}
-        color='danger'
-        onClick={()=> setSkinInfo(!skinInfo)}
-        text={`Skin info ${skinInfo ? 'on' : 'off'}`}
-        />
-        <Button
-        color='warning'
-        onClick={()=> setShowHint(!hint)}
-        text={`Show hint ${hint ? 'off' : 'on'}`}
-        />
-        </div>
-        
-        
+            <h1 className='text-white text-center'>Valordle!</h1>
+            <h2 className='text-white text-center'>A Valorant Skins Wordle Game by <a href='https://github.com/andrewjkim745'>andrewjkim745</a></h2>
+            <p className='text-white text-center'>Updates at 00:00 GMT-10</p>
+            <div className='w-100 d-flex flex-column justify-content-center align-items-center'>
+                <MDBInput onChange={(e) => setInputValue(e.target.value)} value={inputValue} label='Weapon Name' id='form1' type='text' contrast />
+                <Dropdown />
+                <Button
+                    toggle={'toggle'}
+                    color='danger'
+                    onClick={() => setSkinInfo(!skinInfo)}
+                    text={`Skin info ${skinInfo ? 'on' : 'off'}`}
+                />
+                <Button
+                    color='warning'
+                    onClick={() => setShowHint(!hint)}
+                    text={`Show hint ${hint ? 'off' : 'on'}`}
+                />
+            </div>
         </div>
     )
 
